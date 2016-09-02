8d25b548fd38c46ea2dd9acfca45dc1ca66cd5c5
Alex O. Holcombe <aoholcombe@gmail.com>
2016-09-02 12:12:29 +1000

    Tentative start

---
+++ dot-jump.py
@@ -0,0 +1,171 @@
+from __future__ import print_function
+__author__ = """Alex "O." Holcombe, Charles Ludowici, """ ## double-quotes will be silently removed, single quotes will be left, eg, O'Connor
+
+try:
+  import helpersAOH
+except:
+  print 'Could not load helpersAOH'
+  quit
+  
+import psychopy.info
+from psychopy import visual, sound, monitors, logging
+
+refreshRate= 85 *1.0;  #160 #set to the framerate of the monitor
+fullscrn=1; #show in small window (0) or full screen (1) 
+scrn=0 #which screen to display the stimuli. 0 is home screen, 1 is second screen
+# create a dialog from dictionary 
+infoFirst = { 'Autopilot':autopilot, 'Check refresh etc':True, 'Screen to use':scrn, 'Fullscreen (timing errors if not)': fullscrn, 'Screen refresh rate': refreshRate }
+OK = gui.DlgFromDict(dictionary=infoFirst, 
+    title='MOT', 
+    order=['Autopilot','Check refresh etc', 'Screen to use', 'Screen refresh rate', 'Fullscreen (timing errors if not)'], 
+    tip={'Check refresh etc': 'To confirm refresh rate and that can keep up, at least when drawing a grating',
+            'Screen to use': '0 means primary screen, 1 means second screen'},
+    )
+if not OK.OK:
+    print('User cancelled from dialog box'); logging.info('User cancelled from dialog box'); core.quit()
+autopilot = infoFirst['Autopilot']
+checkRefreshEtc = infoFirst['Check refresh etc']
+scrn = infoFirst['Screen to use']
+print('scrn = ',scrn, ' from dialog box')
+fullscrn = infoFirst['Fullscreen (timing errors if not)']
+refreshRate = infoFirst['Screen refresh rate']
+
+#monitor parameters
+widthPix = 1024 #1440  #monitor width in pixels
+heightPix =768  #900 #monitor height in pixels
+monitorwidth = 40.5 #28.5 #monitor width in centimeters
+viewdist = 55.; #cm
+pixelperdegree = widthPix/ (atan(monitorwidth/viewdist) /np.pi*180)
+bgColor = [-1,-1,-1] #black background
+monitorname = 'testMonitor' # 'mitsubishi' #in psychopy Monitors Center
+
+mon = monitors.Monitor(monitorname,width=monitorwidth, distance=viewdist)#fetch the most recent calib for this monitor
+mon.setSizePix( (widthPix,heightPix) )
+myWin = openMyStimWindow(mon,widthPix,heightPix,bgColor,allowGUI,units,fullscr,scrn,waitBlank)
+myMouse = event.Mouse(visible = 'true',win=myWin)
+myWin.setRecordFrameIntervals(False)
+
+mon = monitors.Monitor(monitorname,width=monitorwidth, distance=viewdist)#fetch the most recent calib for this monitor
+mon.setSizePix( (widthPix,heightPix) )
+myWin = openMyStimWindow(mon,widthPix,heightPix,bgColor,allowGUI,units,fullscr,scrn,waitBlank)
+myMouse = event.Mouse(visible = 'true',win=myWin)
+myWin.setRecordFrameIntervals(False)
+
+trialsPerCondition = 2 #default value
+
+refreshMsg2 = ''
+if not checkRefreshEtc:
+    refreshMsg1 = 'REFRESH RATE WAS NOT CHECKED'
+    refreshRateWrong = False
+else: #checkRefreshEtc
+    runInfo = psychopy.info.RunTimeInfo(
+            # if you specify author and version here, it overrides the automatic detection of __author__ and __version__ in your script
+            #author='<your name goes here, plus whatever you like, e.g., your lab or contact info>',
+            #version="<your experiment version info>",
+            win=myWin,    ## a psychopy.visual.Window() instance; None = default temp window used; False = no win, no win.flips()
+            refreshTest='grating', ## None, True, or 'grating' (eye-candy to avoid a blank screen)
+            verbose=True, ## True means report on everything 
+            userProcsDetailed=True  ## if verbose and userProcsDetailed, return (command, process-ID) of the user's processes
+            )
+    print('Finished runInfo- which assesses the refresh and processes of this computer')
+    refreshMsg1 = 'Median frames per second ='+ str( np.round(1000./runInfo["windowRefreshTimeMedian_ms"],1) )
+    refreshRateTolerancePct = 3
+    pctOff = abs( (1000./runInfo["windowRefreshTimeMedian_ms"]-refreshRate) / refreshRate)
+    refreshRateWrong =  pctOff > (refreshRateTolerancePct/100.)
+    if refreshRateWrong:
+        refreshMsg1 += ' BUT'
+        refreshMsg1 += ' program assumes ' + str(refreshRate)
+        refreshMsg2 =  'which is off by more than' + str(round(refreshRateTolerancePct,0)) + '%!!'
+    else:
+        refreshMsg1 += ', which is close enough to desired val of ' + str( round(refreshRate,1) )
+    myWinRes = myWin.size
+    myWin.allowGUI =True
+
+myWin.close() #have to close window to show dialog box
+dlgLabelsOrdered = list() #new dialog box
+myDlg = gui.Dlg(title="object tracking experiment", pos=(200,400))
+if not autopilot:
+    myDlg.addField('Subject name :', subject, tip='or subject code')
+    dlgLabelsOrdered.append('subject')
+myDlg.addField('Trials per condition (default=' + str(trialsPerCondition) + '):', trialsPerCondition, tip=str(trialsPerCondition))
+dlgLabelsOrdered.append('trialsPerCondition')
+pctCompletedBreak = 50
+myDlg.addText(refreshMsg1, color='Black')
+if refreshRateWrong:
+    myDlg.addText(refreshMsg2, color='Red')
+msgWrongResolution = ''
+if checkRefreshEtc and (not demo) and (myWinRes != [widthPix,heightPix]).any():
+    msgWrongResolution = 'Instead of desired resolution of '+ str(widthPix)+'x'+str(heightPix)+ ' pixels, screen apparently '+ str(myWinRes[0])+ 'x'+ str(myWinRes[1])
+    myDlg.addText(msgWrongResolution, color='Red')
+    print(msgWrongResolution); logging.info(msgWrongResolution)
+myDlg.addText('Note: to abort press ESC at a trials response screen', color=[-1.,1.,-1.]) # color='DimGrey') color names stopped working along the way, for unknown reason
+myDlg.show()
+if myDlg.OK: #unpack information from dialogue box
+   thisInfo = myDlg.data #this will be a list of data returned from each field added in order
+   if not autopilot:
+       name=thisInfo[dlgLabelsOrdered.index('subject')]
+       if len(name) > 0: #if entered something
+         subject = name #change subject default name to what user entered
+       trialsPerCondition = int( thisInfo[ dlgLabelsOrdered.index('trialsPerCondition') ] ) #convert string to integer
+       print('trialsPerCondition=',trialsPerCondition)
+       logging.info('trialsPerCondition ='+str(trialsPerCondition))
+else: 
+   print('User cancelled from dialog box.'); logging.info('User cancelled from dialog box')
+   logging.flush()
+   core.quit()
+
+if os.path.isdir('.'+os.sep+'dataRaw'):
+    dataDir='dataRaw'
+else:
+    msg= 'dataRaw directory does not exist, so saving data in present working directory'
+    print(msg); logging.info(msg)
+    dataDir='.'
+expname = ''
+fileNameWithPath = dataDir+'/'+subject+ '_' + expname+timeAndDateStr
+if not demo and not exportImages:
+    saveCodeCmd = 'cp \'' + sys.argv[0] + '\' '+ fileNameWithPath + '.py'
+    os.system(saveCodeCmd)  #save a copy of the code as it was when that subject was run
+    logF = logging.LogFile(fileNameWithPath+'.log', 
+        filemode='w',#if you set this to 'a' it will append instead of overwriting
+        level=logging.INFO)#info, data, warnings, and errors will be sent to this logfile
+if demo or exportImages: 
+  logging.console.setLevel(logging.ERROR)  #only show this level  messages and higher
+logging.console.setLevel(logging.WARNING) #DEBUG means set the console to receive nearly all messges, INFO is for everything else, INFO, EXP, DATA, WARNING and ERROR 
+if refreshRateWrong:
+    logging.error(refreshMsg1+refreshMsg2)
+else: logging.info(refreshMsg1+refreshMsg2)
+longerThanRefreshTolerance = 0.27
+longFrameLimit = round(1000./refreshRate*(1.0+longerThanRefreshTolerance),3) # round(1000/refreshRate*1.5,2)
+msg = 'longFrameLimit='+ str(longFrameLimit) +' Recording trials where one or more interframe interval exceeded this figure '
+logging.info(msg); print(msg)
+if msgWrongResolution != '':
+    logging.error(msgWrongResolution)
+
+myWin = openMyStimWindow(mon,widthPix,heightPix,bgColor,allowGUI,units,fullscr,scrn,waitBlank)
+msg='Window opened'; print(msg); logging.info(msg)
+myMouse = event.Mouse(visible = 'true',win=myWin)
+msg='Mouse enabled'; print(msg); logging.info(msg)
+runInfo = psychopy.info.RunTimeInfo(
+        win=myWin,    ## a psychopy.visual.Window() instance; None = default temp window used; False = no win, no win.flips()
+        refreshTest='grating', ## None, True, or 'grating' (eye-candy to avoid a blank screen)
+        verbose=True, ## True means report on everything 
+        userProcsDetailed=True  ## if verbose and userProcsDetailed, return (command, process-ID) of the user's processes
+        )
+msg = 'second window opening runInfo mean ms='+ str( runInfo["windowRefreshTimeAvg_ms"] )
+logging.info(msg); print(msg)
+logging.info(runInfo)
+logging.info('gammaGrid='+str(mon.getGammaGrid()))
+logging.info('linearizeMethod='+str(mon.getLinearizeMethod()))
+
+
+def oneFrameOfStim():
+	pass
+	
+def oneTrial():
+	#number of locations
+	#SOA, duration
+	pass
+
+while trialNum < trials.nTotal and expStop==False:
+	pass
+	
